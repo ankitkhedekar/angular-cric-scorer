7b6776c83b849881c08d1d4ebae65af95eb819fa
ankitkhedekar <ankitkhedekar@outlook.com>
2020-10-25 23:22:29 +0530

    Test cases fixed

--- src/app/common/scorer-test.js
+++ src/app/common/scorer-test.js
@@ -1,14 +1,14 @@
-import { describe } from 'riteway';
+import { describe } from "riteway";
 
-import Scorer from './scorer';
+import Scorer from "./scorer";
 
-describe('Testing scorer intialisation', async assert => {
+describe("Testing scorer intialisation", async assert => {
   const testScorer = new Scorer();
 
   assert({
-    given: 'no arguments',
-    should: 'return initial score',
-    actual: testScorer.getScore(),
+    given: "no arguments",
+    should: "return initial score",
+    actual: testScorer.score,
     expected: {
       runs: 0,
       wickets: 0,
@@ -18,15 +18,15 @@ describe('Testing scorer intialisation', async assert => {
   });
 });
 
-describe('Testing score reset',  async assert => {
+describe("Testing score reset", async assert => {
   const testScorer = new Scorer();
 
   testScorer._resetScore();
 
   assert({
-    given: 'no arguments',
-    should: 'return initial score',
-    actual: testScorer.getScore(),
+    given: "no arguments",
+    should: "return initial score",
+    actual: testScorer.score,
     expected: {
       runs: 0,
       wickets: 0,
